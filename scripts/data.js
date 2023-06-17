9eb6978f20bcb2cb6f9cacc197e488dfffe6e126
webmmr <mdmizanur3950@gmail.com>
2023-06-18 02:02:04 +0600

    full project complete

--- scripts/data.js
+++ scripts/data.js
@@ -1,19 +1,27 @@
+const screenWidth = window.screen.width;
+
 const data = [
   {
     id: 0,
-    image: "images/desktop-image-hero-0.jpg",
+    image: `images/${
+      screenWidth < 575 ? "mobile" : "desktop"
+    }-image-hero-0.jpg`,
     title: "Discover innovative ways to decorate",
     copy: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
   },
   {
     id: 1,
-    image: "images/desktop-image-hero-1.jpg",
+    image: `images/${
+      screenWidth < 575 ? "mobile" : "desktop"
+    }-image-hero-1.jpg`,
     title: "We are available all across the globe",
     copy: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
   },
   {
     id: 2,
-    image: "images/desktop-image-hero-2.jpg",
+    image: `images/${
+      screenWidth < 575 ? "mobile" : "desktop"
+    }-image-hero-2.jpg`,
     title: "Manufactured with the best materials",
     copy: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to   ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
   },
---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+import data from "./data.js";
+
+// Getting DOM elements
+const sliderImage = document.getElementById("slider-image");
+const heroText = document.getElementById("hero-text");
+const heroCopy = document.getElementById("hero-copy");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const hamBtn = document.getElementById("mobile-nav-open-btn");
+const closeBtn = document.getElementById("mobile-nav-close-btn");
+const mobNav = document.querySelector(".mobile-nav");
+
+// Open mobile navigation
+function openMobileNav() {
+  mobNav.style.transform = "translateX(0)";
+}
+
+// Close mobile navigation
+function closeMobileNav() {
+  mobNav.style.transform = "translateX(-100%)";
+}
+
+// Render the current slide
+function renderSlide(currentSlide) {
+  const { image, title, copy } = data[currentSlide];
+  sliderImage.src = image;
+  heroText.textContent = title;
+  heroCopy.textContent = copy;
+}
+
+// Update the state of prev and next buttons
+function updateButtons() {
+  prevBtn.disabled = currentSlide === 0;
+  nextBtn.disabled = currentSlide === totalSlides - 1;
+}
+
+let currentSlide = 0;
+const totalSlides = data.length;
+prevBtn.setAttribute("disabled", "");
+renderSlide(currentSlide);
+
+// Event listener for previous button
+prevBtn.addEventListener("click", function () {
+  if (currentSlide > 0) {
+    currentSlide--;
+    renderSlide(currentSlide);
+    updateButtons();
+  }
+});
+
+// Event listener for next button
+nextBtn.addEventListener("click", function () {
+  if (currentSlide < totalSlides - 1) {
+    currentSlide++;
+    renderSlide(currentSlide);
+    updateButtons();
+  }
+});
+
+// Event listener for opening mobile navigation
+hamBtn.addEventListener("click", openMobileNav);
+
+// Event listener for closing mobile navigation
+closeBtn.addEventListener("click", closeMobileNav);
